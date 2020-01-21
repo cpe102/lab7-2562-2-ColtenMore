ac6b48f359fd9ed849e00ab647c0fc2bac7c9282
Aungpao <aungpao321@gmail.com>
2020-01-21 16:02:37 +0700

    v1

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,1 +1,43 @@
 //status = "ONE-NIGHT-STAND";
+#include<iostream>
+using namespace std;
+
+int main(){
+    string status;
+    int age,money,h;
+    cout<<"Input Age: ";
+    cin>>age;
+    if(age<=20){
+        cout<<"Input Height: ";
+        cin>>h;
+        if(h<=160){
+            status = "UNFRIEND";
+        }else{
+            if(h<=175){
+                status = "FRIEND";
+            }else{
+                cout<<"Input Wealth(Million): ";
+                cin>>money;
+                if(money>69){
+                    status = "MARRIED";
+                }else{
+                    status = "ONE-NIGHT-STAND";
+                }
+            }
+        }
+    }else{
+        if(age<30){
+            cout<<"Input Wealth(Million): ";
+            cin>>money;
+            if(money>10){
+                status = "BEST FRIEND";
+            }else{
+                status = "UNFRIEND";
+            }
+        }else{
+            status = "UNFRIEND";
+        }
+    }
+    cout<<status;
+    return 0;
+}
